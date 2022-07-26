3019465d152cf56d4ac7547bcbba52865338ac4b
kunalbohra25 <kunal@aladinntech.in>
2022-07-26 11:25:25 +0530

    testing commit 2

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -1,27 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>
         heelo </h1>
-        <h1>
+    <h1>
         heelo </h1>
-        <h1>
-        heelo Sunil bro2</h1>
-        <h1>
+    <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam fugiat alias adipisci quaerat, minima officiis
+        ducimus explicabo, quia ipsam quae vitae, labore quo excepturi recusandae quisquam! Obcaecati odio voluptatem
+        exercitationem.</h1>
+    <h1>
         heelo </h1>
-            <h1>
+    <h1>
         heelo </h1>
-        <h1>
+    <h1>
         heelo </h1>
-        <h1>
+    <h1>
         heelo </h1>
-        <h1>
+    <h1>
         heelo </h1>
 </body>
+
 </html>
