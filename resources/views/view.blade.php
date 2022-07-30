7e82fbc54e54a7ead877e722897b65ff9848414e
kunalbohra25 <kunal@aladinntech.in>
2022-07-30 17:15:37 +0530

    30-07

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -33,28 +33,10 @@
     <h1>
         ugiugiugiu necessitatibus temporibus itaque, est placeat? Facere mollitia reiciendis sed nihil sint aliquam,
         impedit et?heeloheeloheeloheeloheeloheelohe</h1>
+     
+        
     <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur delectus cumque, ea suscipit ad aut vitae velit
-        accusamus sequi dolor facere sunt aperiam recusandae sed, nulla, explicabo libero corporis tempore dolores quia
-        soluta reprehenderit similique fuga? Minima, magsdsadsani, maxime perferendis impedit molestiae nostrum voluptatibus
-        tempore maiores tenetur veritatis enim quas dolorum facilis magnam eum ut? Natus consectetur, beatae enim
-        quisquam, fugiat tempora veniam ipsum itaque pariatur nam eaque expedita veritatis non doloribus. Fuga dolore
-        maiores obcaecati, adipisci molestias voluptatibus mollitia. Rerum laboriosam eum, molestiae nam ipsum, qui
-        facere quis quas blanditiis quam voluptates? Voluptates quisquam modi quos deleniti velit magni voluptatibus ea
-        consectetur ipsa ut exercitationem quod tenetur, eum veniam! Nemo optio quidem odio culpa sunt dolor obcaecati
-        molestiae officia omnis suscipit, natus molestias accusantium impedit eligendi? Unde odio amet, ab
-        necessitatibus ratione quisquam accusantium culpa. Unde, modi illo! Id ea est, voluptas distinctio laborum, nam
-        quibusdam vitae iure voluptates ullam rem. Eos ut atque animi, rem, voluptates pariatur vel, reprehenderit magni
-        eveniet mollitia natus repellendus. Tempora, sunt error possimus reprehenderit corporis perspiciatis vero
-        similique tenetur unde, incidunt architecto praesentium quibusdam laboriosam asperiores. Veritatis esse iure,
-        accusamus officiis labore quod pariatur cum, eum iusto deleniti nostrum soluta? Excepturi, fuga sint. Sunil is a good boy
-    </p>
-
-
-
-
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur delectus cumque, ea suscipit ad aut vitae velit
+       sunil is a good boy Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur delectus cumque, ea suscipit ad aut vitae velit
         accusamus sequi dolor facere sunt aperiam recusandae sed, nulla, explicabo libero corporis tempore dolores quia
         soluta reprehenderit similique fuga? Minima, magni, maxime perferendis impedit molestiae nostrum voluptatibus
         tempore maiores tenetur veritatis enim quas dolorum facilis magnam eum ut? Natus consectetur, beatae enim
